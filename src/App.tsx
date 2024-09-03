d527e65135ccb55f1052240732225c1c9fc3d158
Oleg Bareev <oleg.bareev@yandex.ru>
2024-09-03 18:49:38 +0300

    Add Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Ref, useContext, useState } from "react";
+import { ChangeEvent, MouseEventHandler, Ref, useContext, useState } from "react";
 import "./App.css";
 
 import { FabricJSCanvas } from "./FabricCanvas";
@@ -11,11 +11,11 @@ function App() {
   return (
     <div className="App">
       <CanvasContextProvider>
-        <>
+        <div>
           <FabricJSCanvas />
 
-          <LoadImageButton />
-        </>
+          <Sidebar />
+        </div>
       </CanvasContextProvider>
     </div>
   );
@@ -42,6 +42,39 @@ function LoadImageButton() {
     <div>
       <div>{file && `${file.name} - ${file.type}`}</div>
       <input onChange={handleChangeInputFile} type="file" accept="image/png, image/jpeg" />
+    </div>
+  )
+}
+
+function Sidebar() {
+  const [files, setFiles] = useState<string[]>([]);
+  const { canvas } = useCanvasContext();
+
+  const handleChangeInputFile = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      const newFiles = [...files, URL.createObjectURL(file)];
+
+      setFiles(newFiles);
+    }
+  };
+
+  const handleClickImage = (objURL: string): MouseEventHandler<HTMLImageElement> => {
+    return async () => {
+      const img = await fabric.FabricImage.fromURL(objURL);
+
+      canvas?.add(img);
+    }
+  }
+
+  return (
+    <div>
+      <div>Choose from</div>
+
+      {files.map(objURL => <img key={objURL} src={objURL} alt="Image" onClick={handleClickImage(objURL)} />)}
+
+      <input onChange={handleChangeInputFile} type="file" accept="image/png, image/jpeg" />
+
     </div>
   )
 }
